36cdadb7ce43e237a6286c4f21b80605974395ea
9bini <jinhoa52@gmail.com>
2021-03-19 05:27:40 +0900

    1부 JUnit 5 / JUnit 5 테스트 반복하기 2부

--- src/test/java/me/whiteship/inflearnthejavatest/StudyTest.java
+++ src/test/java/me/whiteship/inflearnthejavatest/StudyTest.java
@@ -2,6 +2,8 @@ package me.whiteship.inflearnthejavatest;
 
 import org.junit.jupiter.api.*;
 import org.junit.jupiter.api.condition.*;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
 
 import static org.assertj.core.api.Assertions.*;
 import static org.junit.jupiter.api.Assumptions.assumeTrue;
@@ -31,8 +33,25 @@ class StudyTest {
         System.out.println("create1");
     }
 
+    @DisplayName("스터디 만들기 RepeatedTest")
+    @RepeatedTest(value = 10, name = "{displayName}, {currentRepetition}/{totalRepetitions}")
+    void repeatTest(RepetitionInfo repetitionInfo){
+        System.out.println("test" + repetitionInfo.getCurrentRepetition() + "/" + repetitionInfo.getTotalRepetitions());
+    }
+
+    // Junit 5에서는 그냥 지원해주고
+    // Junit 4는 서브 파티를 이용해야 사용 가능하다
+    // @ParameterizedTest
+
     // test 실행 전 한번만 실행된다.
     // 작성 방법은 static 추가, 리턴 타입 void, 메소드명은 자유
+    @DisplayName("스터디 만들기 ParameterizedTest")
+    @ParameterizedTest(name = "{index} {displayName} message={0}")
+    @ValueSource(strings = {"날씨가", "많이", "추워지고", "있네요."})
+    void parameterizedTest(String message){
+        System.out.println("message = " + message);
+    }
+
     @BeforeAll
     static void beforeAll() {
         System.out.println("before all");
