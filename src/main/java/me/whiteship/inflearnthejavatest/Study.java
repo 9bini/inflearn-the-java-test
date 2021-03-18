4cd20cfbfbd267d0303f40eb4f646b120df51e52
9bini <jinhoa52@gmail.com>
2021-03-19 05:43:03 +0900

    1부 JUnit 5 / JUnit 5 테스트 반복하기 2부

--- src/main/java/me/whiteship/inflearnthejavatest/Study.java
+++ src/main/java/me/whiteship/inflearnthejavatest/Study.java
@@ -2,11 +2,22 @@ package me.whiteship.inflearnthejavatest;
 
 public class Study {
 
-    private StudyStatus status = StudyStatus.DRAFT;
+    private StudyStatus status;
     private int limit = -1;
 
+    private String name;
+
+    public Study(int limit, String name) {
+        this.limit = limit;
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+
     public Study(int limit) {
-        if (limit <0){
+        if (limit < 0) {
             throw new IllegalArgumentException("limit은 0보다 커야한다.");
         }
         this.limit = limit;
@@ -18,5 +29,14 @@ public class Study {
 
     public int getLimit() {
         return limit;
+    }
+
+    @Override
+    public String toString() {
+        return "Study{" +
+                "status=" + status +
+                ", limit=" + limit +
+                ", name='" + name + '\'' +
+                '}';
     }
 }
--- src/test/java/me/whiteship/inflearnthejavatest/StudyTest.java
+++ src/test/java/me/whiteship/inflearnthejavatest/StudyTest.java
@@ -2,10 +2,21 @@ package me.whiteship.inflearnthejavatest;
 
 import org.junit.jupiter.api.*;
 import org.junit.jupiter.api.condition.*;
+import org.junit.jupiter.api.extension.ParameterContext;
 import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.aggregator.AggregateWith;
+import org.junit.jupiter.params.aggregator.ArgumentsAccessor;
+import org.junit.jupiter.params.aggregator.ArgumentsAggregationException;
+import org.junit.jupiter.params.aggregator.ArgumentsAggregator;
+import org.junit.jupiter.params.converter.ArgumentConversionException;
+import org.junit.jupiter.params.converter.ConvertWith;
+import org.junit.jupiter.params.converter.SimpleArgumentConverter;
+import org.junit.jupiter.params.provider.CsvSource;
 import org.junit.jupiter.params.provider.ValueSource;
+import org.junit.jupiter.params.shadow.com.univocity.parsers.annotations.Convert;
 
 import static org.assertj.core.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assumptions.assumeTrue;
 import static org.junit.jupiter.api.Assumptions.assumingThat;
 
@@ -35,7 +46,7 @@ class StudyTest {
 
     @DisplayName("스터디 만들기 RepeatedTest")
     @RepeatedTest(value = 10, name = "{displayName}, {currentRepetition}/{totalRepetitions}")
-    void repeatTest(RepetitionInfo repetitionInfo){
+    void repeatTest(RepetitionInfo repetitionInfo) {
         System.out.println("test" + repetitionInfo.getCurrentRepetition() + "/" + repetitionInfo.getTotalRepetitions());
     }
 
@@ -47,9 +58,34 @@ class StudyTest {
     // 작성 방법은 static 추가, 리턴 타입 void, 메소드명은 자유
     @DisplayName("스터디 만들기 ParameterizedTest")
     @ParameterizedTest(name = "{index} {displayName} message={0}")
-    @ValueSource(strings = {"날씨가", "많이", "추워지고", "있네요."})
-    void parameterizedTest(String message){
-        System.out.println("message = " + message);
+//    @ValueSource(ints = {10, 20, 40})
+    @CsvSource({"10, '자바 스터디'", "20, 스프링"})
+//    void parameterizedTest(@ConvertWith(StudyConverter.class) Study study) {
+//            System.out.println("study.getLimit() = " + study.getLimit());
+
+    /*void parameterizedTest(int limit, String name){
+        System.out.println(new Study(limit, name));*/
+
+    void parameterizedTest(@AggregateWith(StudyAggregator.class) Study study){
+        System.out.println("study = " + study);
+    }
+
+    // 제약 조건 public class 이야하고 static을 붙여줘야한다.
+    static class StudyAggregator implements ArgumentsAggregator{
+        @Override
+        public Object aggregateArguments(ArgumentsAccessor argumentsAccessor, ParameterContext parameterContext) throws ArgumentsAggregationException {
+            Study study = new Study(argumentsAccessor.getInteger(0), argumentsAccessor.getString(1));
+            return study;
+        }
+    }
+
+    // 하나만 적용 가능하다.
+    static class StudyConverter extends SimpleArgumentConverter{
+        @Override
+        protected Object convert(Object source, Class<?> targetType) throws ArgumentConversionException {
+            assertEquals(Study.class, targetType, "Can only convert to Study");
+            return new Study(Integer.parseInt(source.toString()));
+        }
     }
 
     @BeforeAll
